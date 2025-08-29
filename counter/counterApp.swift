e7db9f8ce70aabc04486c06fbd88167347c9ce7c
m1rage <76838335+AshutoshKr17@users.noreply.github.com>
2025-08-29 19:11:58 +0530

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Ashutosh Kumar Kushwaha on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ counterTests/counterTests.swift
@@ -0,0 +1,17 @@
+//
+//  counterTests.swift
+//  counterTests
+//
+//  Created by Ashutosh Kumar Kushwaha on 29/08/25.
+//
+
+import Testing
+@testable import counter
+
+struct counterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
